0b3b35b6085178df1e376e956e906a9241cb294a
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2023-05-17 19:45:17 -0400

    Added EnginaMachine Solution

---
+++ Enigma/Enigma/EnigmaCypherMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    internal class EnigmaCypherMachine
+    {
+        // Represents physical Enginma Cypher Machine
+
+
+
+    }
+}
---
+++ Enigma/Enigma/EnigmaMain.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Landon Buell
+ * May 2023
+ */
+
+public class EnigmaMain
+{
+    static void Main(string[] args)
+    {
+        // Main
+    }
+}
